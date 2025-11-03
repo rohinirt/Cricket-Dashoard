05edea3a4c76479c961d12d49969da6db3bb5eb9
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2025-11-03 19:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ with col1:
                 x0=-1.5, x1=1.5,
                 y0=params["y0"], y1=params["y1"],
                 fillcolor=params["color"],
-                opacity=0.95,
+                opacity=0.5,
                 layer="below",
                 line_width=0,
             )
@@ -203,8 +203,8 @@ with col1:
                 text=f"<b>Pitch Map - {batsman_name}</b>", 
                 x=0, y=0.95, font=dict(size=20)
             ),
-            width=250, 
-            height=500, # Increased height for better visualization of lengths
+            width=100, 
+            height=400, # Increased height for better visualization of lengths
             xaxis=dict(
                 range=[-1.5, 1.5],
                 showgrid=False, zeroline=False,visible = False
