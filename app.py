696b9ca023ff3feaafea4e7f015a3a88b6e4d351
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2025-11-04 19:46:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ with col2:
             norm = mcolors.Normalize(vmin=0, vmax=1)
         
         # Choose the Colormap (I'll switch back to 'Reds' as 'Blues' was used in the Zonal Chart)
-        cmap = cm.get_cmap('Green')
+        cmap = cm.get_cmap('green')
         
         # Map the run totals to colors
         colors = cmap(norm(runs))
